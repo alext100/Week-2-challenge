b6af13c633e2262a7502bc5a21c85634252be2d0
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-10-08 18:30:39 +0200

    initial matriz and nextGeneration function created

---
+++ src/script.js
@@ -0,0 +1,55 @@
+/* Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.
+At each step in time, the following transitions occur:
+
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbours becomes a live cell, asif by reproduction.
+
+These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
+
+1. Any live cell with two or three live neighbours survives.
+2. Any dead cell with three live neighbours becomes a live cell.
+3. All other live cells die in the next generation.Similarly, all other dead cells stay dead.
+The initial pattern constitutes the seed of the system.The first generation is created by applying the above rules simultaneously to every 
+cell in the seed, live or dead;
+births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick.Each generation is a pure
+function of the preceding one.The rules
+continue to be applied repeatedly to create further generations. */
+
+const initialMatriz = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function nextGeneration(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+        for (j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j] === 1) {
+                if (numberOfLivingNeighbors === 2 || numberOfLivingNeighbors === 3) {
+                    matriz[i][j] = 1;
+                } else {
+                    matriz[i][j] = 0;
+                }
+
+            } else if (matriz[i][j] === 0) {
+                if (numberOfLivingNeighbors === 3) {
+                    matriz[i][j] = 1
+                }
+            }
+        }
+    }
+
+
+
+
+    return nextMatriz;
+}
